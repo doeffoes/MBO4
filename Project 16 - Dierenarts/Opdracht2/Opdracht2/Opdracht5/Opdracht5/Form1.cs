919d1baa397b75e05afe1c2371c700c1954d605e
doeffoes <doeffoegaming@gmail.com>
2025-02-08 21:49:01 +0100

    Add files via upload

---
+++ Project 16 - Dierenarts/Opdracht2/Opdracht2/Opdracht5/Opdracht5/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Opdracht5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // maken verbinding
+            OleDbConnection con = new OleDbConnection();
+            globaal.Connection.Open();
+
+            // opstellen query
+            string sql = "SELECT COUNT(*) FROM Categorie";
+            string sql2 = "SELECT COUNT(*) FROM Recept";
+            string sql3 = "SELECT COUNT(*) FROM Recept HAVING Bereidingstijd <= 30";
+
+            // uitvoeren query
+            OleDbCommand dbcom = new OleDbCommand(sql, globaal.Connection);
+            OleDbCommand dbcom2 = new OleDbCommand(sql2, globaal.Connection);
+            OleDbCommand dbcom3 = new OleDbCommand(sql3, globaal.Connection);
+
+            // lezen gegevens uit database
+            textBox1.Text = dbcom.ExecuteScalar().ToString();
+            textBox2.Text = dbcom2.ExecuteScalar().ToString();
+            textBox3.Text = dbcom3.ExecuteScalar().ToString();
+
+            // sluiten verbinding
+            globaal.Connection.Close();
+        }
+
+        private void Overzichtklanten()
+        {
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Project 16 - Dierenarts/Opdracht2/Opdracht2/Opdracht5/Opdracht5/globaal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace Opdracht5
+{
+    class globaal
+    {
+        // vaste connectie naar database
+        public static OleDbConnection Connection = new OleDbConnection
+            (@"Provider=Microsoft.ACE.OLEDB.12.0;
+            data source = ..\\..\\..\\Recepten.accdb ; 
+            Persist Security Info=False;");
+    }
+}
