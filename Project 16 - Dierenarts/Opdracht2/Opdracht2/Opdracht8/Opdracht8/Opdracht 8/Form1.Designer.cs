2f3cb2fd58a35bb713c0d5ffeae82c12c9d3763e
doeffoes <doeffoegaming@gmail.com>
2025-02-08 21:51:22 +0100

    Add files via upload

---
+++ Project 16 - Dierenarts/Opdracht2/Opdracht2/Opdracht8/Opdracht8/Opdracht 8/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Opdracht8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbxOverzicht = new System.Windows.Forms.ListBox();
+            this.btnVerwijder = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbxOverzicht);
+            this.groupBox1.Location = new System.Drawing.Point(29, 42);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(469, 228);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Overzicht Recepten";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // lbxOverzicht
+            // 
+            this.lbxOverzicht.FormattingEnabled = true;
+            this.lbxOverzicht.ItemHeight = 16;
+            this.lbxOverzicht.Location = new System.Drawing.Point(21, 32);
+            this.lbxOverzicht.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lbxOverzicht.Name = "lbxOverzicht";
+            this.lbxOverzicht.Size = new System.Drawing.Size(420, 180);
+            this.lbxOverzicht.TabIndex = 0;
+            this.lbxOverzicht.SelectedIndexChanged += new System.EventHandler(this.lbxOverzicht_SelectedIndexChanged);
+            // 
+            // btnVerwijder
+            // 
+            this.btnVerwijder.Location = new System.Drawing.Point(389, 309);
+            this.btnVerwijder.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnVerwijder.Name = "btnVerwijder";
+            this.btnVerwijder.Size = new System.Drawing.Size(109, 31);
+            this.btnVerwijder.TabIndex = 5;
+            this.btnVerwijder.Text = "Verwijderen";
+            this.btnVerwijder.UseVisualStyleBackColor = true;
+            this.btnVerwijder.Click += new System.EventHandler(this.btnVerwijder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 416);
+            this.Controls.Add(this.btnVerwijder);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Firma Bruinsma";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lbxOverzicht;
+        private System.Windows.Forms.Button btnVerwijder;
+    }
+}
+
---
+++ Project 16 - Dierenarts/Opdracht2/Opdracht2/Opdracht8/Opdracht8/Opdracht 8/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Opdracht8
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+        }
+
+        private void lbxOverzicht_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string woord = lbxOverzicht.SelectedItem.ToString();
+            string[] woordenArray = woord.Split(' ');
+            int Receptid = int.Parse(woordenArray[0]);
+            LadenKlantDetails(Receptid);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+           LeesGegevens();
+        }
+        private void LadenKlantDetails(int Receptid)
+        {
+                OleDbDataReader dbread = null;
+                OleDbConnection con = new OleDbConnection();
+                Globaal.Connection.Open();
+                string sql = "SELECT * FROM Recept";
+                OleDbCommand dbcom = new OleDbCommand(sql, Globaal.Connection);
+                dbread = dbcom.ExecuteReader();
+                while (dbread.Read())
+                {
+                     dbread.GetValue(1).ToString();
+                     dbread.GetValue(2).ToString();
+                     dbread.GetValue(3).ToString();
+                     dbread.GetValue(4).ToString();
+                     dbread.GetValue(5).ToString();
+                     dbread.GetValue(6).ToString();
+                     dbread.GetValue(7).ToString();
+                }
+                dbread.Close();
+                dbcom.Dispose();
+                Globaal.Connection.Close();
+           
+             
+        }
+        public void LeesGegevens()
+        {
+                OleDbDataReader dbread = null;
+                OleDbConnection con = new OleDbConnection();
+                Globaal.Connection.Open();
+                string sql = "SELECT * FROM Recept";
+                OleDbCommand dbcom = new OleDbCommand(sql, Globaal.Connection);
+                dbread = dbcom.ExecuteReader();
+                while (dbread.Read())
+                {
+                    lbxOverzicht.Items.Add(dbread.GetValue(0).ToString() + " " 
+                       +dbread.GetValue(1).ToString() + " " + 
+                        dbread.GetValue(2).ToString() + " " +
+                        dbread.GetValue(3).ToString() + " " +
+                        dbread.GetValue(4).ToString() + " " +
+                        dbread.GetValue(5).ToString() + " " +
+                        dbread.GetValue(6).ToString() + " " +
+                        dbread.GetValue(7).ToString());
+                }
+                dbread.Close();
+                dbcom.Dispose();
+                Globaal.Connection.Close();
+        }
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+                
+        }
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            LeesGegevens();
+        }
+        private void btnVerwijder_Click(object sender, EventArgs e)
+        {
+            string woord = lbxOverzicht.SelectedItem.ToString();
+            string[] woordenArray = woord.Split(' ');
+            int Receptid = int.Parse(woordenArray[0]);
+            VerwijderKlantgegevens(Receptid);
+        }
+        public void VerwijderKlantgegevens(int Receptid)
+        {
+            Globaal.Connection.Open();
+            OleDbConnection con = new OleDbConnection();
+            string sql = "DELETE FROM Recept WHERE [Recept-id] = @Receptid";
+            OleDbCommand dbcom = new OleDbCommand(sql, Globaal.Connection);
+            dbcom.Parameters.AddWithValue("@Recept-id", Receptid);
+            dbcom.ExecuteNonQuery();
+            dbcom.Dispose();
+            Globaal.Connection.Close();
+            MessageBox.Show("Verwijderd");
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
