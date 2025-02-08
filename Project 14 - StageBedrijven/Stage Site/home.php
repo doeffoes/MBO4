741e10f5d17c533618bd4147693faa456844ac09
doeffoes <doeffoegaming@gmail.com>
2025-02-08 21:24:45 +0100

    Project 14 - StageBedrijven

---
+++ Project 14 - StageBedrijven/Stage Site/home.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  
+
+  <title>Stage</title>
+  <style type="text/css">
+   table {
+       border-collapse: collapse;
+       width: 100%;
+       color: #588c7e;
+       font-family: monospace;
+       font-size: 25px;
+       text-align: right;
+   }
+   </style>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand bg-dark navbar-dark">
+    <img src='images/logo-nova-college.png'>
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="home.html">Home</a>
+      </li>
+    </ul>
+  </nav>
+
+  <table>
+    <th>bedrijfsnummer</th>
+    <th>stage_id</th>
+    <th>schooljaar</th>
+    <th>soort_stage</th>
+    <th>voornaam</th>
+    <th>begin_datum</th>
+  </table>
+
+  <?php
+   $conn = mysqli_connect("localhost", "root", "Moermond57", "stage");
+   if ($conn-> connect_error) {
+       die("Connection failed:". $conn-> connect_error);
+   }
+  
+   $sql = "SELECT bedrijfsnummer, stage_id, schooljaar, soort_stage, voornaam, begin_datum from stage";
+   $result = $conn-> query($sql);
+
+   if ($result-> num_rows > 0) {
+       while ($row = $result-> fetch_assoc ()) {
+           echo "<tr><td>". $row["bedrijfsnummer"] ."</tr></td>". $row["stage_id"] ."</tr></td>". $row["schooljaar"] ."</tr></td>". $row["soort_stage"] ."</tr></td>". $row["voornaam"] ."</tr></td>". $row[
+           "begin_datum"] ."</tr></td>";
+           
+
+           
+       }
+       echo "</table>";
+   }
+   else {
+       echo "0 result";
+   }
+   $conn-> close();
+
+   
+   $conn = mysqli_connect("localhost", "root", "Moermond57", "bedrijf");
+   if ($conn-> connect_error) {
+       die("Connection failed:". $conn-> connect_error);
+   }
+  
+   $sql = "SELECT bedrijfsnummer, bedrijfsnaam, adres, postcode, woonplaats, telefoon_algemeem, alg_email, website, SD_accreditatie, stage_2yr from bedrijf";
+   $result = $conn-> query($sql);
+
+   if ($result-> num_rows > 0) {
+       while ($row = $result-> fetch_assoc ()) {
+           echo "<tr><td>". $row["bedrijfsnummer"] ."</tr></td>". $row["bedrijfsnaam"] ."</tr></td>". $row["adres"] ."</tr></td>". $row["postcode"] ."</tr></td>". $row["woonplaats"] ."</tr></td>". $row[
+           "telefoon_algemeen"] ."</tr></td>". $row["alg_email"] ."</tr></td>". $row["website"] ."</tr></td>". $row["SD_accreditatie"] ."</tr></td>". $row["stage_2yr"];
+           
+
+           
+       }
+       echo "</table>";
+   }
+   else {
+       echo "0 result";
+   }
+   $conn-> close();
+  ?>
+  
+  
+
+
+
+
+  
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
