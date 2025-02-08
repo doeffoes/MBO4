1d0bd951cfddd81770bf187b9eef3707fd3bf3ad
doeffoes <doeffoegaming@gmail.com>
2025-02-08 21:50:28 +0100

    Add files via upload

---
+++ Project 16 - Dierenarts/Opdracht2/Opdracht2/Opdracht7/Opdracht7/Opdracht7/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Opdracht7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbxOverzicht_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string woord = lbxOverzicht.SelectedItem.ToString();
+            string[] woordenArray = woord.Split(' ');
+            int klantid = int.Parse(woordenArray[0]);
+            LadenKlantDetails(klantid);
+        }
+
+        private void btnNieuweKlant_Click(object sender, EventArgs e)
+        {
+            FrmKlantInvoer frm = new FrmKlantInvoer();
+            frm.ShowDialog();
+        }
+
+        public void WijzigKlantgegevens(int klantid)
+        {
+            globaal.Connection.Open();
+            OleDbConnection con = new OleDbConnection();
+            string sql = "UPDATE Klanten SET Voorletters = @Voorletters, " +
+                "Tussenvoegsel = @Tussenvoegsel, Achternaam = @Achternaam, " +
+                "Straat = @Straat, Huisnummer = @Huisnummer, Postcode = @Postcode, " +
+                "Woonplaats = @Woonplaats, Telefoon = @Telefoon WHERE klantid = " +
+                "@klantid";
+            OleDbCommand dbcom = new OleDbCommand(sql, globaal.Connection);
+            dbcom.Parameters.AddWithValue("@Voorletters", txtVoorletters.Text);
+            dbcom.Parameters.AddWithValue("@Tussenvoegsel", txtVoorvoegsel.Text);
+            dbcom.Parameters.AddWithValue("@Achternaam", txtAchternaam.Text);
+            dbcom.Parameters.AddWithValue("@Straat", txtStraat.Text);
+            dbcom.Parameters.AddWithValue("@Huisnummer", txtHuisnummer.Text);
+            dbcom.Parameters.AddWithValue("@tHuisnummer", txtTHuisnummer.Text);
+            dbcom.Parameters.AddWithValue("@Postcode", txtPostcode.Text);
+            dbcom.Parameters.AddWithValue("@Woonplaats", txtWoonplaats.Text);
+            dbcom.Parameters.AddWithValue("@Telefoon", txtWoonplaats.Text);
+            dbcom.Parameters.AddWithValue("@klantid", klantid);
+            dbcom.ExecuteNonQuery();
+            dbcom.Dispose();
+            globaal.Connection.Close();
+            MessageBox.Show("Gewijzigd");
+        }
+
+        public void LeesGegevens()
+        {
+            OleDbDataReader dbread = null;
+            OleDbConnection con = new OleDbConnection();
+            globaal.Connection.Open();
+            string sql = "SELECT * FROM klanten";
+            OleDbCommand dbcom = new OleDbCommand(sql, globaal.Connection);
+            dbread = dbcom.ExecuteReader();
+            while (dbread.Read())
+            {
+                lbxOverzicht.Items.Add(dbread.GetValue(0).ToString() + " "
+                    + dbread.GetValue(1).ToString() + " " +
+                    dbread.GetValue(2).ToString() + " " +
+                    dbread.GetValue(3).ToString());
+            }
+            dbread.Close();
+            dbcom.Dispose();
+            globaal.Connection.Close();
+
+        }
+
+        private void LadenKlantDetails(int klantid)
+        {
+
+            OleDbDataReader dbread = null;
+            OleDbConnection con = new OleDbConnection();
+            globaal.Connection.Open();
+            string sql = "SELECT * FROM klanten WHERE klantid = @klantid";
+            OleDbCommand dbcom = new OleDbCommand(sql, globaal.Connection);
+            OleDbParameter param1 = new OleDbParameter();
+            param1.ParameterName = "@klantid";
+            param1.Value = klantid;
+            dbcom.Parameters.Add(param1);
+            dbread = dbcom.ExecuteReader();
+            while (dbread.Read())
+            {
+                txtVoorletters.Text = dbread.GetValue(1).ToString();
+                txtVoorvoegsel.Text = dbread.GetValue(2).ToString();
+                txtAchternaam.Text = dbread.GetValue(3).ToString();
+                txtStraat.Text = dbread.GetValue(4).ToString();
+                txtHuisnummer.Text = dbread.GetValue(5).ToString();
+                txtWoonplaats.Text = dbread.GetValue(6).ToString();
+                txtPostcode.Text = dbread.GetValue(7).ToString();
+                txtWoonplaats.Text = dbread.GetValue(8).ToString();
+                txtTelefoon.Text = dbread.GetValue(9).ToString();
+            }
+            dbread.Close();
+            dbcom.Dispose();
+            globaal.Connection.Close();
+
+
+        }
+
+    }
+}
---
+++ Project 16 - Dierenarts/Opdracht2/Opdracht2/Opdracht7/Opdracht7/Opdracht7/FrmKlantInvoer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Opdracht7
+{
+    public partial class FrmKlantInvoer : Form
+    {
+        public FrmKlantInvoer()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmKlantInvoer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInvoeren_Click(object sender, EventArgs e)
+        {
+            string voorletters = txtVoorletters.Text;
+            string voorvoegsel = txtVoorvoegsel.Text;
+            string achternaam = txtAchternaam.Text;
+            string straat = txtStraat.Text;
+            int huisnummer = int.Parse(txtHuisnummer.Text);
+            string tHuisnummer = txtTHuisnummer.Text;
+            string postcode = txtPostcode.Text;
+            string woonplaats = txtWoonplaats.Text;
+            string telefoon = txtTelefoon.Text;
+
+            OleDbConnection con = new OleDbConnection();
+            globaal.Connection.Open();
+
+
+            //string sql1 = "INSERT INTO Klanten (Voorletters,Tussenvoegsel,Achternaam,Straat,Huisnummer,[Toevoeging_huisnummer],Postcode,Woonplaats,Telefoon) VALUES ('" + voorletters + "','" + voorvoegsel + "','" + achternaam + "','" + straat + "','" + huisnummer + "','" + tHuisnummer+ "','" + postcode + "','" + woonplaats + "','" + telefoon + "')";
+            string sql1 = "INSERT INTO Klanten (Voorletters,Tussenvoegsel,Achternaam,Straat,Huisnummer,[Toevoeging_huisnummer],Postcode,Woonplaats,Telefoon) VALUES (@Voorletters, @Tussenvoegsel, @Achternaam,@Straat, @Huisnummer, @tHuisnummer, @Postcode, @Woonplaats, @Telefoon )";
+            OleDbCommand dbcom2 = new OleDbCommand(sql1, globaal.Connection);
+
+            dbcom2.Parameters.AddWithValue("@Voorletters", voorletters);
+            dbcom2.Parameters.AddWithValue("@Tussenvoegsel", voorvoegsel);
+            dbcom2.Parameters.AddWithValue("@Achternaam", achternaam);
+            dbcom2.Parameters.AddWithValue("@Straat", straat);
+            dbcom2.Parameters.AddWithValue("@Huisnummer", huisnummer);
+            dbcom2.Parameters.AddWithValue("@tHuisnummer", tHuisnummer);
+            dbcom2.Parameters.AddWithValue("@Postcode", postcode);
+            dbcom2.Parameters.AddWithValue("@Woonplaats", woonplaats);
+            dbcom2.Parameters.AddWithValue("@Telefoon", telefoon);
+            dbcom2.ExecuteNonQuery();
+
+            dbcom2.Dispose();
+            globaal.Connection.Close();
+            MessageBox.Show("Ingevoerd");
+            this.Close();
+            Form1.ActiveForm.Show();
+        }
+    }
+}
