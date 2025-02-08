eda53bc419a820863f6a218a926de862182b0242
doeffoes <doeffoegaming@gmail.com>
2025-02-08 21:42:15 +0100

    Add files via upload

---
+++ Project 16 - Dierenarts/Opdracht2/Opdracht2/Opdracht2/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Opdracht2MericRecepten
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbxOverzicht = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbxOverzicht);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(604, 361);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Overzicht Recepten";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // lbxOverzicht
+            // 
+            this.lbxOverzicht.FormattingEnabled = true;
+            this.lbxOverzicht.ItemHeight = 16;
+            this.lbxOverzicht.Location = new System.Drawing.Point(84, 60);
+            this.lbxOverzicht.Name = "lbxOverzicht";
+            this.lbxOverzicht.Size = new System.Drawing.Size(409, 228);
+            this.lbxOverzicht.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lbxOverzicht;
+    }
+}
+
---
+++ Project 16 - Dierenarts/Opdracht2/Opdracht2/Opdracht2/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Opdracht2MericRecepten
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // maken verbinding
+            OleDbConnection con = new OleDbConnection();
+            Globaal.Connection.Open();
+
+            // opstellen query
+            string sql = "SELECT * FROM Recept";
+
+            // uitvoeren query
+            OleDbCommand dbcom = new OleDbCommand(sql, Globaal.Connection);
+            OleDbDataReader dbread = null;
+
+            // lezen gegevens uit database
+            dbread = dbcom.ExecuteReader();
+            while (dbread.Read())
+            {
+                lbxOverzicht.Items.Add(dbread.GetValue(1).ToString()
+                );
+            }
+
+            // sluiten verbinding
+            dbread.Close();
+            Globaal.Connection.Close();
+        }
+    }
+}
